b8dd6faf15d004a8e867af1543329114e6b7205e
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-05-17 20:42:51 +0545

    Create Validating_input_conversion_with_assert().dart

---
+++ Validating_input_conversion_with_assert().dart
@@ -0,0 +1,9 @@
+void main() {
+    String ten = "10";
+    String threepointfourteen = "3.14";
+    int Ten = int.parse(ten);
+    double Threepointfourteen = double.parse(threepointfourteen);
+    assert (Ten == 10);
+    assert (Threepointfourteen == "3.14");
+    print($Ten and $Threepointfourteen is the answer to this exersise);
+}
