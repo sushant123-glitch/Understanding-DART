044665bc4f6ce1b9e507ecdf22765abd4088ebad
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-05-22 11:45:34 +0545

    Create Challenge_to_create_a_function.dart

---
+++ Challenge_to_create_a_function.dart
@@ -0,0 +1,11 @@
+///Challenge to create a function
+
+void greetUser() {
+    print("Hello! welcome to Dart functions");
+}
+
+/// Now let's use the function which we have created
+
+void main() {
+    greetUser();
+}
