99be68b2607e1772f1305ddd9ea53b35d254973d
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-05-22 12:05:09 +0545

    Create Challenge_personalized_day_message.dart

---
+++ Challenge_personalized_day_message.dart
@@ -0,0 +1,10 @@
+/// Challenge (Personalized day message)
+
+void greetWithDay(String name, String day) {
+    print("Hello, $name! Happy $day");
+}
+
+///using two parameters
+void main() {
+    greetWithDay("Coffeeholic", "Thursday");
+}
