c3af5044a5d8a4e7edca87144d8f4284f18b84e6
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-05-22 11:53:25 +0545

    Create Challenge_to_create_a_function_with_parameters.dart

---
+++ Challenge_to_create_a_function_with_parameters.dart
@@ -0,0 +1,11 @@
+/// Challenge to create a function with parameters
+
+void customGreeting (String name) {
+    print("Hello, $name have a great day!");
+}
+
+///using a function with parameters
+
+void main() {
+    customGreeting("Coffeeholic");
+}
