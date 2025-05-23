9854f9f0f0b0b128632dd7157ae8cdc170e82a36
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-05-23 12:02:26 +0545

    Create Challenge_Multiply_two_numbers_using_function.dart

---
+++ Challenge_Multiply_two_numbers_using_function.dart
@@ -0,0 +1,12 @@
+/// Challenge Multiply two numbers using function
+
+int multiply (int a, int b) {
+    return (a*b);    
+}
+
+/// Calling the function
+
+void main() {
+    int result= multiply(10,10);
+    print("The result is: $result");
+}
