4a7f368b6da5f920747d32f9e3573b16c8934b43
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-05-28 12:46:15 +0545

    Create Challenge_Favorite_movie_list.dart

---
+++ Challenge_Favorite_movie_list.dart
@@ -0,0 +1,11 @@
+/// Challenge_Favorite_movie_list
+
+void main () {
+    List<String>favoriteMovies = ["Maleficient", "John Wick", "Tears of a sun"];
+    print(favoriteMovies);
+    print(favoriteMovies[0]);
+    favoriteMovies.add("Saving Private Ryan");
+    favoriteMovies.remove("John Wick");
+    print(favoriteMovies);
+    print(favoriteMovies.length);
+}
