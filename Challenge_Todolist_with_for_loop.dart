81889cfe87a589eaead8151c33a26984a0cfdcff
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-06-03 12:06:04 +0545

    Create Challenge_Todolist_with_for_loop.dart

---
+++ Challenge_Todolist_with_for_loop.dart
@@ -0,0 +1,20 @@
+/// Challenge creating todolist with for loop
+
+import "dart:io";
+
+void main() {
+
+  ///Creating a list
+  
+  List<String>todoList=[];
+
+  ///Creating user input using for loop
+  for (int i=1; i<=3; i++) {
+    print("Enter Task $i:");
+    String? task = stdin.readLineSync();
+}
+
+/// only adds non-empty task
+
+
+}
