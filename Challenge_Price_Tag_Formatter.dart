25cc02470b3f527dcca4e9a9d412604a8a203efc
sushant123-glitch <84083451+sushant123-glitch@users.noreply.github.com>
2025-06-05 11:23:00 +0545

    Update Challenge_Price_Tag_Formatter.dart

--- Challenge_Price_Tag_Formatter.dart
+++ Challenge_Price_Tag_Formatter.dart
@@ -4,6 +4,13 @@ void main() {
 List<double>priceTag=[9.99,25.50,100.0,49.75];
 
 ///Converting priceTag which is double into string using map()
-String updated =priceTag.toStringAsFixed(2);
-print ("$updated");
+
+List<String>stringPriceTag=priceTag.map((newList){
+    return "Rs." + newList.toStringAsFixed(2);
+}).toList();
+int c=1;
+stringPriceTag.forEach((updated){
+   print("$c. $updated");
+   c=c+1; 
+});
 }
